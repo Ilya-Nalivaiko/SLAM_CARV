2db6f19c7397bef59484e2a2b1a82ac8451e7f75
IslamAAli <islam.a.mustafa@gmail.com>
2024-07-22 13:01:42 -0600

    adding run script

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 docker run -d -p 5900:5900 --rm --network host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --device=/dev/video0:/dev/video0 --env QT_X11_NO_MITSHM=1 -it islamaali/slam_carv-docker:v1.0
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rosrun ORB_CARV_Pub Mono Vocabulary/ORBvoc.txt Logitech_c270_HD720p.yaml /camera/image_raw:=/usb_cam/image_raw
