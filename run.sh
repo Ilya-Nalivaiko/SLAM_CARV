2c7180be1b12c4cfe915d118aae4a2b4aab41ce6
IslamAAli <islam.a.mustafa@gmail.com>
2024-07-22 13:04:24 -0600

    adding run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun ORB_CARV_Pub Mono Vocabulary/ORBvoc.txt Logitech_c270_HD720p.yaml /camera/image_raw:=/usb_cam/image_raw
+rosrun ORB_CARV_Pub Mono Vocabulary/ORBvoc.txt Logitech_c270_HD720p.yaml /camera/image_raw:=/camera/image_raw
