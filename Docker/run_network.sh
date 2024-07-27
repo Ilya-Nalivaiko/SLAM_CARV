0f99798b6dd8f8677b5db5abe786523fb0653efe
IslamAAli <islam.a.mustafa@gmail.com>
2024-07-26 22:48:02 -0600

    fix yaml file

--- Docker/run_network.sh
+++ Docker/run_network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 5900:5900 --rm --network host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --device=/dev/video0:/dev/video0 --env QT_X11_NO_MITSHM=1 -it islamaali/slam_carv-docker:v1.0
+docker run -d -p 5900:5900 -m 4g --cpus="4.0" --rm --network host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --device=/dev/video0:/dev/video0 --env QT_X11_NO_MITSHM=1 -it islamaali/slam_carv-docker:v1.0
