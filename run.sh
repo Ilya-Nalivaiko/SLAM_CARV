716fe1d059ee7a40bd1eff08d313b8a72a46ee77
Ilya-Nalivaiko <65912861+Ilya-Nalivaiko@users.noreply.github.com>
2025-06-28 16:57:20 -0600

    fix config file location

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun ORB_CARV_Pub Mono Vocabulary/ORBvoc.txt Logitech_c270_HD720p.yaml /camera/image_raw:=/camera/image_raw
+rosrun ORB_CARV_Pub Mono Vocabulary/ORBvoc.txt config_files/Logitech_c270_HD720p.yaml /camera/image_raw:=/camera/image_raw
