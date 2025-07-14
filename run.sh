9ff5589bc43eb1345876c1edaf9b6e1655340903
Ilya-Nalivaiko <65912861+Ilya-Nalivaiko@users.noreply.github.com>
2025-07-14 12:17:45 -0600

    makes running it faster

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rosrun ORB_CARV_Pub Mono Vocabulary/ORBvoc.txt config_files/Logitech_c270_HD720p.yaml 192.168.1.75 8080 192.168.1.75 5555
+rosrun ORB_CARV_Pub Mono Vocabulary/ORBvoc.txt config_files/Logitech_c270_HD720p.yaml 192.168.1.133 8080 192.168.1.133 5555
